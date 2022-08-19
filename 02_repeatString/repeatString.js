3a0d1e4155d94a60b5ea655f26e310bae3e71365
Oscar Elmahdy <opelm1@gmail.com>
2022-08-19 18:28:35 +0800

    Solution to first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  const output = [""];
+  for (i = 0; i < num; ++i) {
+    output[i] = string;
+  }
+  return output.join("");
 };
 
 // Do not edit below this line
