9e9f3bc1f6d76cbd97ebfef9a18e8d8e66d003fb
Oscar Elmahdy <opelm1@gmail.com>
2022-08-21 00:52:07 +0800

    Include provided solution which uses rest operator

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,5 +5,11 @@ const removeFromArray = function ([a, b, c, d], rm1, rm2, rm3, rm4) {
   return arr.filter((item) => !remove.includes(item));
 };
 
+// Provided solution:
+const solution = function (...args) {
+  const array = args[0];
+  return array.filter((val) => !args.includes(val));
+};
+
 // Do not edit below this line
 module.exports = removeFromArray;
