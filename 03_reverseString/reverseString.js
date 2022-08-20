9dab4c307d8c4f1537120868105f3483e37e1a1c
Oscar Elmahdy <opelm1@gmail.com>
2022-08-20 13:31:38 +0800

    Add solution, passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  // const array = [""]; // might need this. don't forget to remove other decl.
+  // const array = string.split("");
+  // for (i = array.length - 1; i >= 0; --i) {
+  // array.reverse();
+  // }
+  return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
