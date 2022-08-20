577e29d07411ec51d72a2eca072e9bc40fd61224
Oscar Elmahdy <opelm1@gmail.com>
2022-08-21 00:34:45 +0800

    Add solution, passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function ([a, b, c, d], rm1, rm2, rm3, rm4) {
+  const arr = [a, b, c, d];
+  const remove = [rm1, rm2, rm3, rm4];
 
+  return arr.filter((item) => !remove.includes(item));
 };
 
 // Do not edit below this line
