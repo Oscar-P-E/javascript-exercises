15c6f0bec5b42791e635354b0a19f123cb959c4e
Oscar Elmahdy <opelm1@gmail.com>
2022-08-24 17:31:50 +0800

    Add solution, passes all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,22 @@
-const ftoc = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
+// let rounded = Math.round(number * 10) / 10;
+
+const ftoc = function (f) {
+  let c = (f - 32) * (5 / 9);
+  return round(c, 1);
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let f = c * (9 / 5) + 32;
+  return round(f, 1);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
