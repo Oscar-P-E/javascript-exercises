bf666a99a7f3a4fa63085708af181cbbeebbc943
Oscar Elmahdy <opelm1@gmail.com>
2022-09-22 15:08:31 +0800

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const forwards = string.toLowerCase().replace(/[^a-z]/g, "");
+  const backwards = forwards.split("").reverse().join("");
+  if (forwards === backwards) {
+    return true;
+  }
+  return false;
+  // return console.log(forwards + "\n" + backwards);
 };
 
 // Do not edit below this line
