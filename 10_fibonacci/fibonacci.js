586c5d8758b4bb7f6af081b6242ee79f3f2a753e
Oscar Elmahdy <opelm1@gmail.com>
2022-09-23 20:39:08 +0800

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+// 1, 1, 2, 3, 5, 8
 
+const fibonacci = function (member) {
+  // loop that starts at 1 and adds prev number, 'member' times
+  let thisN = 1;
+  let backOne = 1;
+  let backTwo = 1;
+
+  if (member < 1) {
+    return "OOPS";
+  }
+
+  if (member < 3) {
+    return thisN;
+  }
+
+  for (let i = 3; i <= member; ++i) {
+    thisN = backOne + backTwo;
+    backTwo = backOne;
+    backOne = thisN;
+  }
+  return thisN;
 };
 
 // Do not edit below this line
